4c07a3935291a65d1f691278ea7bbd2fa2fbab09
DongHyun <ehdgus6709@naver.com>
2023-02-28 15:21:04 +0900

    study

---
+++ myWeb/src/main/java/chap07/Calc.java
@@ -0,0 +1,52 @@
+package chap07;
+
+public class Calc {
+	private int num1;
+	private int num2;
+	private String operator;
+	
+	public Calc() {
+		
+	}
+	
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	public int getNum1() {
+		return this.num1;
+	}
+	public int getNum2() {
+		return this.num2;
+	}
+	public String getOperator() {
+		return this.operator;
+	}
+	
+	public int calcResult() {
+		int result = 0;
+		
+		if(this.operator != null) {
+			if(this.operator.equals("+")) {
+				result = this.num1 + this.num2;
+			}
+			else if(this.operator.equals("-")) {
+				result = this.num1 - this.num2;
+			}
+			else if(this.operator.equals("*")) {
+				result = this.num1 * this.num2;
+			}
+			else if(this.operator.equals("/")) {
+				result = this.num1 / this.num2;
+			}
+		}
+		return result;
+	}
+}
